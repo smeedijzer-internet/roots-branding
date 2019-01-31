a8a4eaa620b97c5d33527cb3209fb4b11d397c4b
Mickaël OUILLON <mickael.ouillon@gmail.com>
2019-01-31 15:20:10 +0100

    Update Branding with custom RusseBlanc + SOIL

--- src/Init.php
+++ src/Init.php
@@ -14,8 +14,68 @@ class Init
         add_action('wp_before_admin_bar_render', [$this, 'menu_custom_logo']);
         add_filter('login_headerurl', [$this, 'login_logo_url']);
         add_filter('login_headertitle', [$this, 'login_logo_title']);
-    }
 
+        // IF SOIL
+        /**
+         * Enable features from Soil when plugin is activated
+         * @link https://roots.io/plugins/soil/
+         */
+        add_theme_support('soil-clean-up');
+        add_theme_support('soil-disable-asset-versioning');
+        add_theme_support('soil-disable-trackbacks');
+        add_theme_support('soil-jquery-cdn');
+        add_theme_support('soil-nav-walker');
+        add_theme_support('soil-nice-search');
+        add_theme_support('soil-relative-urls');
+
+        add_action('phpmailer_init', [$this, 'disable_xmailer']);
+        function disable_xmailer($phpmailer) {
+            $phpmailer->XMailer = ' ';
+        }
+
+        // THUMBNAIL SIZE & QUALITY
+        function my_custom_jpeg_quality()
+        {
+            return 100;
+        }
+        add_filter('jpeg_quality', [$this, 'my_custom_jpeg_quality']);
+
+        // Hide WordPress Version Info
+        remove_action('wp_head', [$this, 'wp_generator']);
+
+        // Remove WordPress Version Number In URL Parameters From JS/CSS
+        function hide_wordpress_version_in_script($src, $handle)
+        {
+            $src = remove_query_arg('ver', $src);
+            return $src;
+        }
+        add_filter( 'style_loader_src', [$this, 'hide_wordpress_version_in_script'], 10, 2 );
+        add_filter( 'script_loader_src', [$this, 'hide_wordpress_version_in_script'], 10, 2 );
+
+        /* ADMIN CONFIG */
+        // Custom admin login header logo
+        function msk_custom_admin_color_palette()
+        {
+            wp_admin_css_color(
+                'msk-colors',
+                __('RusseBlanc'),
+                App\asset_path('styles/wp-admin.css'),
+                array('rgb(213, 43, 30)', 'rgb(31, 31, 31)', 'rgb(213, 43, 30)', 'rgb(255, 255, 255)'),
+                array('rgb(213, 43, 30)', 'rgb(31, 31, 31)', 'rgb(213, 43, 30)', 'rgb(255, 255, 255)')
+            );
+        }
+        add_action('admin_init', [$this, 'msk_custom_admin_color_palette']);
+
+        function msk_default_admin_color_palette($user_id) {
+            $args = array(
+            'ID' => $user_id,
+            'admin_color' => 'msk-colors'
+            );
+
+            wp_update_user($args);
+        }
+        add_action('user_register', [$this, 'msk_default_admin_color_palette']);
+    }
 
     /**
     * Remove WordPress admin bar menu
@@ -24,7 +84,6 @@ class Init
     {
         $wp_admin_bar->remove_node('wp-logo');
     }
-
 
     /**
     * Replace login screen logo
@@ -43,14 +102,12 @@ class Init
   <?php
     }
 
-
-
     /**
      * Replace login screen logo link
      */
     public function login_logo_url($url)
     {
-        return 'https://roots.io';
+        return 'https://wwww.russeblanc.com';
     }
 
 
@@ -58,7 +115,7 @@ class Init
     // Replace login logo title
     public function login_logo_title()
     {
-        return 'Powered by Roots';
+        return 'Propulsé avec ❤️ par <a href="https://www.russeblanc.com" style="color:#d52b1e" target="_blank">RUSSE<strong>BLANC</strong></a>';
     }
 
 
@@ -76,8 +133,8 @@ class Init
         $wp_admin_bar->add_node([
           'parent' => $menu_id,
           'title' => __('Homepage'),
-          'id' => 'my-logo-home',
-          'href' => 'https://roots.io',
+          'id' => 'RusseBlanc',
+          'href' => 'https://wwww.russeblanc.com',
           'meta' => ['target' => '_blank']
           ]);
     }
@@ -108,6 +165,6 @@ class Init
     */
     public function admin_footer($content)
     {
-        return 'Site powered by <a href="https://roots.io">Roots</a>';
+        return 'Imaginé & développé avec ❤️ par <a href="https://www.russeblanc.com" style="color:#d52b1e" target="_blank">RUSSE<strong>BLANC</strong></a>';
     }
 }
